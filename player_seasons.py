e38ef3a0436c35734b019d26e839e1ac21a89a6e
Niloufar Neinavaii <nilotus9@gmail.com>
2025-09-27 22:21:34 +0330

    get players.csv and player_seasons.csv

---
+++ player_seasons.py
@@ -0,0 +1,84 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+def get_player_data(browser,team,season_list):
+    print(team)
+    player_seasons = []
+    players = []
+    for season_year, season_url in season_list:
+        per_game_data_rows = []
+        browser.get(season_url)
+        per_game_table = browser.find_element(By.CSS_SELECTOR, 'table#per_game_stats')
+        per_game_tbody = per_game_table.find_element(By.TAG_NAME, 'tbody')
+        per_game_rows = per_game_tbody.find_elements(By.TAG_NAME, 'tr')
+        per_game_thead = per_game_table.find_element(By.TAG_NAME, 'thead')
+        per_game_header_row = per_game_thead.find_element(By.TAG_NAME, 'tr')
+        per_game_headers = [th.text for th in per_game_header_row.find_elements(By.TAG_NAME, 'th')]
+        for row in per_game_rows:
+            cells = row.find_elements(By.XPATH, './*')
+            if not cells:
+                continue
+            per_game_row_data = [cell.text.strip() for cell in cells]
+            per_game_row_data.append(team)
+            per_game_row_data.append(season_year)
+            per_game_data_rows.append(per_game_row_data)
+
+        per_game_headers = per_game_headers + ['Team'] + ['Season']
+        per_game_df = pd.DataFrame(per_game_data_rows, columns=per_game_headers)
+        per_game_df.drop(columns=['Rk','Pos'], inplace=True)
+
+        player_data_rows = []
+        player_table = browser.find_element(By.CSS_SELECTOR, 'table#roster')
+        player_tbody = player_table.find_element(By.TAG_NAME, 'tbody')
+        player_rows = player_tbody.find_elements(By.TAG_NAME, 'tr')
+        player_thead = player_table.find_element(By.TAG_NAME, 'thead')
+        player_header_row = player_thead.find_element(By.TAG_NAME, 'tr')
+        player_headers = [th.text for th in player_header_row.find_elements(By.TAG_NAME, 'th')]
+        for row in player_rows:
+            cells = row.find_elements(By.XPATH, './*')
+            if not cells:
+                continue
+            player_row_data = [cell.text.strip() for cell in cells]
+            player_data_rows.append(player_row_data)
+
+        player_df = pd.DataFrame(player_data_rows, columns=player_headers)
+        player_df.drop(columns='No.', inplace=True)
+
+        salaries_data_rows = []
+        salaries_table = browser.find_element(By.CSS_SELECTOR, 'table#salaries2')
+        salaries_tbody = salaries_table.find_element(By.TAG_NAME, 'tbody')
+        salaries_rows = salaries_tbody.find_elements(By.TAG_NAME, 'tr')
+        salaries_thead = salaries_table.find_element(By.TAG_NAME, 'thead')
+        salaries_header_row = salaries_thead.find_element(By.TAG_NAME, 'tr')
+        salaries_headers = [th.text for th in salaries_header_row.find_elements(By.TAG_NAME, 'th')]
+        salaries_headers[1] = 'Player'
+        for row in salaries_rows:
+            cells = row.find_elements(By.XPATH, './*')
+            if not cells:
+                continue
+            salaries_row_data = [cell.text.strip() for cell in cells]
+            salaries_data_rows.append(salaries_row_data)
+
+        salaries_df = pd.DataFrame(salaries_data_rows, columns=salaries_headers)
+        salaries_df.drop(columns='Rk', inplace=True)
+
+        df = per_game_df.merge(player_df, how='inner', on='Player')
+        df = df.merge(salaries_df, how='left',on='Player')
+        df.drop(columns=['Age','Pos','Ht','Wt','Birth','Birth Date','College','Awards','FG%','3P','3PA','3P%','2P','2PA','2P%'], inplace=True)
+        player_df.drop(columns='Exp', inplace=True)
+
+
+
+        player_seasons.append(df)
+        players.append(player_df)
+    if player_seasons and players:
+        final_player_seasons_df = pd.concat(player_seasons, ignore_index=True)
+        final_players_df = pd.concat(players, ignore_index=True)
+        final_players_df.drop_duplicates(subset=['Player', 'Birth Date'], inplace=True)
+        final_players_df.reset_index(drop=True, inplace=True)
+        return final_player_seasons_df,final_players_df
+    else:
+        print(f"No data found for {team}")
+
+
---
+++ seasons.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from player_seasons import get_player_data
+import pandas as pd
+
+def get_season_data(browser,team_list):
+    player_seasons_data = []
+    players_data = []
+    for team_name, team_url in team_list:
+        browser.get(team_url)
+        team= team_url.split("teams/")[1].split('/')[0]
+        table = browser.find_element(By.CSS_SELECTOR, f'table#{team}')
+        tbody = table.find_element(By.TAG_NAME, 'tbody')
+        rows = tbody.find_elements(By.TAG_NAME, 'tr')
+        season = []
+        for row in rows:
+            cells = row.find_elements(By.XPATH, './*')
+            if not cells:
+                continue
+            first_cell = cells[0]
+            link = first_cell.find_elements(By.TAG_NAME, 'a')
+            if not link:
+                continue
+            season_text = link[0].text.strip()
+            season_link = link[0].get_attribute("href")
+            season.append((season_text, season_link))
+        player_seasons,players = get_player_data(browser,team_name,season[1:8])
+        player_seasons_data.append(player_seasons)
+        players_data.append(players)
+    final_player_seasons_df = pd.concat(player_seasons_data, ignore_index=True)
+    final_player_seasons_df.to_csv("Player_Seasons.csv", index=False)
+
+    final_players_df = pd.concat(players_data, ignore_index=True)
+    final_players_df['id'] = final_players_df.index + 1
+    final_players_df = final_players_df[['id', 'Player', 'Pos', 'Ht', 'Wt', 'Birth Date', 'Birth', 'College']]
+    final_players_df.to_csv("players.csv", index=False)
+    print("Saved")
+
+
+
