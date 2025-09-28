e1636138c78479249968c970a9d441eb65bf7f0b
Mahan_Rezaee <rezaeemahan357@gmail.com>
2025-09-28 19:52:50 +0330

    Charoletee team problem is fixed

--- data_base/query.py
+++ data_base/query.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 conn = sqlite3.connect('nba_database.db')
 
-# write your query here
-query = '''SELECT * FROM players
+# write your query
+query = '''SELECT * FROM player_seasons
+WHERE team_id = 4 and season_id = 3
 LIMIT 5;'''
 df = pd.read_sql_query(query, conn)
 print(df.to_string(index=False))
